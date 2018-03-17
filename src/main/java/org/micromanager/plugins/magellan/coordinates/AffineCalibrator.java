bc16d89315c228cd8f8f350c048c8d32d79c2e52
henrypinkard <henry.pinkard@gmail.com>
2018-03-17 12:28:15 -0700

    Better automatic version of Affine calibnration using least squares fitting and automatic movement. Still room for improvement on algorithm

--- src/main/java/org/micromanager/plugins/magellan/coordinates/AffineCalibrator.java
+++ src/main/java/org/micromanager/plugins/magellan/coordinates/AffineCalibrator.java
@@ -19,27 +19,24 @@ package main.java.org.micromanager.plugins.magellan.coordinates;
 
 import ij.IJ;
 import ij.ImagePlus;
-import ij.ImageStack;
-import ij.process.FloatProcessor;
-import ij.process.ImageConverter;
-import ij3d.image3d.FHTImage3D;
+import ij.WindowManager;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import javax.swing.JOptionPane;
 import main.java.org.micromanager.plugins.magellan.main.Magellan;
 import main.java.org.micromanager.plugins.magellan.misc.Log;
 import mmcorej.CMMCore;
-import mmcorej.TaggedImage;
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.LUDecomposition;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.micromanager.data.Coords;
 import org.micromanager.data.Datastore;
 import org.micromanager.data.Image;
-import main.java.org.micromanager.plugins.magellan.misc.ProgressBar;
+import ij.process.ByteProcessor;
+import ij.process.ShortProcessor;
+import java.util.ArrayList;
 
 
 /**
@@ -49,13 +46,14 @@ import main.java.org.micromanager.plugins.magellan.misc.ProgressBar;
 public class AffineCalibrator {
  
 
-   private CountDownLatch nextImageLatch_ = new CountDownLatch(1);
    private AffineGUI affineGui_ ;
    private volatile boolean abort_ =false;
    private Datastore datastore_;
+   private final double fovDiameter_um_;
    
-   public AffineCalibrator(AffineGUI ag) {
+   public AffineCalibrator(AffineGUI ag, double fovDiam) {
       affineGui_ = ag;
+      fovDiameter_um_ = fovDiam;
       //new thread to not hold up EDT
       new Thread(new Runnable() {
          @Override
@@ -71,13 +69,8 @@ public class AffineCalibrator {
 
    }
    
-   public void readyForNextImage() {
-      nextImageLatch_.countDown();
-   }
-   
    public void abort() {
       abort_ = true;
-      readyForNextImage();
       //close window
       if (datastore_ != null) {
          datastore_.close();
@@ -86,54 +79,43 @@ public class AffineCalibrator {
    }
    
    public void computeAffine() throws Exception {
-      IJ.log("Automatic affine calibration.");
-      IJ.log(" ");
-      IJ.log("This module will attempt to estimate the affine transformation matrix based on \n"
-              + "three images of the same object in a different part of the field of view. Between \n"
-              + "each successive image, the XY stage must be tranlsated so that the object is in a \n"
-              + "different part of the field of view. The first image has already been captured.");
       CMMCore core = Magellan.getCore();
       String xyStage = core.getXYStageDevice();
-      Point2D.Double[] stagePositions = new Point2D.Double[3], pixPositions = new Point2D.Double[3];
       
-      stagePositions[0] = new Point2D.Double(core.getXPosition(xyStage) ,core.getYPosition(xyStage) );
-      Image img0 = snapAndAdd(0);
-      nextImageLatch_ = new CountDownLatch(1);
-      Log.log("Move stage to new position with same features visible, and press Capture");
-      nextImageLatch_.await();
-      if(abort_) {        
-         return;
+      int numImages = 5;
+      Point2D.Double currentPos = new Point2D.Double(core.getXPosition(xyStage), core.getYPosition(xyStage));
+      Point2D.Double[] targetPositions = new Point2D.Double[numImages];
+      targetPositions[0] = currentPos;  
+      for (int i = 0; i < numImages; i++) {
+          double theta = (2*Math.PI / (double) (numImages-1)) * i;
+          double radius = fovDiameter_um_ / 2;
+          targetPositions[i] = new Point2D.Double(currentPos.x + Math.cos(theta)*radius, currentPos.y + Math.sin(theta)*radius);          
+        }
+   
+      Point2D.Double[] stagePositions = new Point2D.Double[numImages];
+      ArrayList<Image> images = new ArrayList<Image>();
+       for (int i = 0; i < numImages; i++) {
+           //move stage to new position
+           core.setXYPosition(targetPositions[i].x, targetPositions[i].y);
+           core.waitForDevice(xyStage);
+           //capture new image, add it to display, and store it
+           stagePositions[i] = new Point2D.Double(core.getXPosition(xyStage), core.getYPosition(xyStage));
+           images.add(snapAndAdd(i));
+           if (abort_) {
+               return;
+           }
       }
-      
-      stagePositions[1] = new Point2D.Double(core.getXPosition(xyStage), core.getYPosition(xyStage));
-      Image img1 = snapAndAdd(1);
-      nextImageLatch_ = new CountDownLatch(1);
-      Log.log("Move stage to new position with same features visible, and press Capture");
-      nextImageLatch_.await();
-      if(abort_) {
-         return;
-      }
-      
-      stagePositions[2] = new Point2D.Double(core.getXPosition(xyStage), core.getYPosition(xyStage));
-      Image img2 = snapAndAdd(2);
-
-      //use Xcorr to calculate pixel coordinates
-      ProgressBar progressBar = new ProgressBar("Computing affine transform (may take several minutes)", 0, 3);
-      progressBar.setProgress(0);
-      progressBar.setVisible(true);
-      pixPositions[0] = new Point2D.Double(0, 0); // define first one as the origin
-      progressBar.setProgress(1);
-      pixPositions[1] = crossCorrelate(img0, img1);
-      progressBar.setProgress(2);
-      pixPositions[2] = crossCorrelate(img0, img2);
-      progressBar.setProgress(3);
-      progressBar.setVisible(false);
-
-
-      //3) compute affine from three pairs of points
-      AffineTransform transform = computeAffine(pixPositions[0], pixPositions[1], pixPositions[2],
-              stagePositions[0], stagePositions[1], stagePositions[2]);
-      
+       
+       //compute pixel shifts relative to first image for all subsequent
+       Point2D.Double[] pixPositions = new Point2D.Double[numImages];
+       pixPositions[0] = new Point2D.Double(0, 0);
+       for (int i = 1; i < numImages; i++) {
+           pixPositions[i] = computePixelShift(images.get(0), images.get(i));
+       }
+       
+       //compute affine transform based on pairs of corresponding points
+       AffineTransform transform = computeAffine(stagePositions, pixPositions);
+ 
      //ask if user likes this affine transform and wants to store it
       int result = JOptionPane.showConfirmDialog(affineGui_, "Calulcated affine transform matrix: " + transform.toString() + ". Would you like to store these settings?",
                "Store calculated affine transform?", JOptionPane.YES_NO_OPTION);
@@ -144,111 +126,116 @@ public class AffineCalibrator {
          AffineUtils.transformUpdated(core.getCurrentPixelSizeConfig(), transform);
       }
    }
-
-   private Point2D.Double crossCorrelate(Image img1, Image img2) throws Exception {    
-      //double the width of iamges used for xCorr to support offsets bigger than half the iage size
-       int width = 2 *img1.getWidth();
-       int height = 2 * img1.getHeight();
-       ImageStack stack1 = new ImageStack(width, height);
-       ImageStack stack2 = new ImageStack(width, height);
-       boolean eightBit = img1.getBytesPerPixel() == 1;
-       Object newPix1, newPix2, sortedPix1, sortedPix2;
-      if (eightBit) {
-         newPix1 = new byte[width * height];
-         newPix2 = new byte[width * height];
-         //fill with background pix value to not throw things off
-         sortedPix1 = Arrays.copyOf((byte[]) img1.getRawPixels(), (width / 2) * (height / 2));
-         sortedPix2 = Arrays.copyOf((byte[]) img2.getRawPixels(), (width / 2) * (height / 2));
-         Arrays.sort((byte[]) sortedPix1);
-         Arrays.sort((byte[]) sortedPix2);
-         Arrays.fill((byte[]) newPix1, ((byte[]) sortedPix1)[(int) (((byte[]) sortedPix1).length * 0.1)]);
-         Arrays.fill((byte[]) newPix2, ((byte[]) sortedPix2)[(int) (((byte[]) sortedPix2).length * 0.1)]);
-      } else {
-         newPix1 = new short[width * height];
-         newPix2 = new short[width * height];
-         //fill with background pix value to not throw things off
-         sortedPix1 = Arrays.copyOf((short[]) img1.getRawPixels(), (width / 2) * (height / 2));
-         sortedPix2 = Arrays.copyOf((short[]) img2.getRawPixels(), (width / 2) * (height / 2));
-         Arrays.sort((short[]) sortedPix1);
-         Arrays.sort((short[]) sortedPix2);
-         Arrays.fill((short[]) newPix1, ((short[]) sortedPix1)[(int) (((short[]) sortedPix1).length * 0.1)]);
-         Arrays.fill((short[]) newPix2, ((short[]) sortedPix2)[(int) (((short[]) sortedPix2).length * 0.1)]);
-      }
-
-      for (int y = 0; y < height / 2; y++) {
-         System.arraycopy(img1.getRawPixels(), y*(width/2), newPix1, (y+height/4)*width + width/4, width/2);
-           System.arraycopy(img2.getRawPixels(), y*(width/2), newPix2, (y+height/4)*width + width/4, width/2);
+   
+   private Point2D.Double computePixelShift(Image img1, Image img2) {
+       String name1 = "ImageToFT1";
+       String name2 = "ImageToFT2";
+       ImagePlus ip1 = getImagePlus(img1, name1);
+       ImagePlus ip2 = getImagePlus(img2, name2);
+           
+       ip1.show();
+       IJ.run("FFT");
+       ip1.close();
+       ip2.show();
+       IJ.run("FFT");
+       ip2.close();
+       IJ.run("FD Math...", "image1=[FFT of "+name1+"] operation=Correlate image2=[FFT of "+name2+"] result=Result do");
+       WindowManager.getImage("FFT of "+name1).close();
+       WindowManager.getImage("FFT of "+name2).close();
+       ImagePlus resultIP = WindowManager.getImage("Result");
+       float[] pix = (float[]) resultIP.getProcessor().getPixels();
+       Point2D.Double maxLocation = new Point2D.Double(0, 0);
+       float maxVal = Float.MIN_VALUE;
+       for (int i =0; i < pix.length; i++) {
+           if (maxVal < pix[i]) {
+               maxVal = pix[i];
+               maxLocation.x = i % resultIP.getWidth();
+               maxLocation.y = i / resultIP.getWidth();
+           }
        }
-       stack1.addSlice(null, newPix1);
-       stack2.addSlice(null, newPix2);
-      ImagePlus ip1 = new ImagePlus("ip1", stack1);
-      ImagePlus ip2 = new ImagePlus("ip2", stack2);
-      //convert to 32 bit floats
-      new ImageConverter(ip1).convertToGray32();
-      new ImageConverter(ip2).convertToGray32();
-      ImageStack xCorrStack = FHTImage3D.crossCorrelation(ip1.getStack(), ip2.getStack());
-      ImagePlus xCorr = new ImagePlus("XCorr", xCorrStack);        
-      double maxVal = xCorr.getStatistics(ImagePlus.MIN_MAX).max;
-          
-//      xCorr.show();
-      //find pixel cooridinates of maxVal
-      for (int x = 0; x < width; x++) {
-         for (int y = 0; y < height; y++) {
-            if (((FloatProcessor) xCorr.getProcessor()).getPixelValue(x, y) == maxVal) {
-               xCorr.close();
-               ip1.close(); 
-               ip2.close();
-               return new Point2D.Double( width/2 - x, height/2 - y);
-            }
-         }
-      }
-      xCorr.close();
-      ip1.close();
-      ip2.close();
-      throw new Exception("Coudnlt find pixel max valu in xCorr Image");
+       //compute relative to center
+       maxLocation.x = maxLocation.x - resultIP.getWidth()/2;
+       maxLocation.y = maxLocation.y - resultIP.getHeight()/2;
+       resultIP.close();
+      return maxLocation;
    }
    
+   
+   private ImagePlus getImagePlus(Image img, String name) {
+          ImagePlus ip = null;
+       if (img.getBytesPerPixel() == 1) {
+           ip = new ImagePlus(name, new ByteProcessor(img.getWidth(), img.getHeight(), (byte[]) img.getRawPixels()));
+       } else {
+           ip = new ImagePlus(name, new ShortProcessor(img.getWidth(), img.getHeight(), (short[]) img.getRawPixels(), null ));
+       }
+       return ip;
+   }
+
    /**
     * 
     * @return pixels
     * @throws Exception 
     */
-   private Image snapAndAdd(int index) throws Exception {
-      boolean liveOn = Magellan.getStudio().live().getIsLiveModeOn();
-      if (liveOn) {
-         Magellan.getStudio().live().setSuspended(true);
-      }
-      List<Image> images = Magellan.getStudio().live().snap(false);
-      if (datastore_ == null) {
-         datastore_ = Magellan.getStudio().displays().show(images.get(0));
-      } else {
-         Coords.CoordsBuilder builder = Magellan.getStudio().data().getCoordsBuilder();
-         builder.time(index); 
-         datastore_.putImage(images.get(0).copyAtCoords(builder.build()));
-      }
-      if (liveOn) {
-          Magellan.getStudio().live().setSuspended(false);
-      }
-      return images.get(0);
-   }
+    private Image snapAndAdd(int index) throws Exception {
+        boolean liveOn = Magellan.getStudio().live().getIsLiveModeOn();
+        if (liveOn) {
+            Magellan.getStudio().live().setSuspended(true);
+        }
+        List<Image> images = Magellan.getStudio().live().snap(false);
+        if (datastore_ == null) {
+            datastore_ = Magellan.getStudio().displays().show(images.get(0));
+        } else {
+            Coords.CoordsBuilder builder = Magellan.getStudio().data().getCoordsBuilder();
+            builder.time(0);
+            builder.channel(0);
+            builder.stagePosition(index);
+            builder.z(0);
+            datastore_.putImage(images.get(0).copyAtCoords(builder.build()));
+        }
+        if (liveOn) {
+            Magellan.getStudio().live().setSuspended(false);
+        }
+        return images.get(0);
+    }
 
    //see http://stackoverflow.com/questions/22954239/given-three-points-compute-affine-transformation
-   private AffineTransform computeAffine(Point2D.Double pix1, Point2D.Double pix2 ,Point2D.Double pix3,
-           Point2D.Double stage1, Point2D.Double stage2, Point2D.Double stage3) {
-      //solve system x' = xA for A, x is stage points and x' is pixel points
-      //6x6 matrix
-      RealMatrix pixPoints = new Array2DRowRealMatrix(new double[][]{
-                 {pix1.x, pix1.y, 1, 0, 0, 0}, {0, 0, 0, pix1.x, pix1.y, 1},
-                 {pix2.x, pix2.y, 1, 0, 0, 0}, {0, 0, 0, pix2.x, pix2.y, 1}, 
-                 {pix3.x, pix3.y, 1, 0, 0, 0}, {0, 0, 0, pix3.x, pix3.y, 1}});
-      //6x1 matrix
-      RealMatrix stagePoints = new Array2DRowRealMatrix(new double[]{stage1.x, stage1.y, stage2.x, stage2.y, stage3.x, stage3.y});
-      //invert stagePoints matrix
-      RealMatrix stagePointsInv = new LUDecomposition(pixPoints).getSolver().getInverse();
-      RealMatrix A = stagePointsInv.multiply(stagePoints);
-      AffineTransform transform = new AffineTransform(new double[]{A.getEntry(0,0),A.getEntry(3,0),A.getEntry(1,0),A.getEntry(4,0)});
-      return transform;
-   }
+   private AffineTransform computeAffine(Point2D.Double[] stagePositions, Point2D.Double[] pixPositions) {
+      // s = Ma
+      //aLS = (M^t * M)^-1 * s
+      
+    //s is a (2 * # of points)x1 columns vector with stage coords
+    double[] sData = new double[2*stagePositions.length];
+    for (int i = 0; i < stagePositions.length; i++) {
+        sData[2*i] = stagePositions[i].x;
+        sData[2*i + 1] = stagePositions[i].y;
+    }
+    RealMatrix s = new Array2DRowRealMatrix(sData);
+      
+    //M is matrix with pixel coords
+    double[][] MData = new double[2*stagePositions.length][6];
+    for (int i = 0; i < stagePositions.length; i++) {
+        //two rows per iteration
+        MData[2*i][0] = pixPositions[i].x;
+        MData[2*i][1] = pixPositions[i].y;
+        MData[2*i][2] = 1;
+        MData[2*i][3] = 0;
+        MData[2*i][4] = 0;
+        MData[2*i][5] = 0;
+        MData[2*i + 1][0] = 0;
+        MData[2*i + 1][1] = 0;
+        MData[2*i + 1][2] = 0;
+        MData[2*i + 1][3] = pixPositions[i].x;
+        MData[2*i + 1][4] = pixPositions[i].y;
+        MData[2*i + 1][5] = 1;
+    }
+      RealMatrix M = new Array2DRowRealMatrix(MData);
+      
+      RealMatrix hatMat = M.transpose().multiply(M);
+      RealMatrix pInv = (new LUDecomposition(hatMat)).getSolver().getInverse().multiply(M.transpose());
+      RealMatrix A = pInv.multiply(s);
+      AffineTransform transform = new AffineTransform(new double[]{A.getEntry(0,0),A.getEntry(1,0),A.getEntry(3,0),A.getEntry(4,0)});
+      return transform;  
+   } 
 
   
 }
