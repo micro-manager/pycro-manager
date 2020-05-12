34a3507ceaf025d2cdc491184a2e88c01c754b51
Henry Pinkard <henry.pinkard@gmail.com>
2020-05-12 09:54:03 -0700

    with last commit

---
+++ test/custom_axis_acq.py
@@ -0,0 +1,19 @@
+import numpy as np
+from pycromanager import Acquisition, multi_d_acquisition_events
+
+
+with Acquisition('/Users/henrypinkard/megllandump', 'l_axis') as acq:
+    #create one event for the image at each z-slice
+    events = []
+    for time in range(5):
+        for index, z_um in enumerate(np.arange(start=0, stop=10, step=0.5)):
+            evt = {
+                    #'axes' is required. It is used by the image viewer and data storage to
+                    #identify the acquired image
+                    'axes': {'l': index, 'time': time},
+
+                    #the 'z' field provides the z position in µm
+                    'z': z_um}
+            events.append(evt)
+
+    acq.acquire(events)
--- test/sandbox.py
+++ test/sandbox.py
@@ -51,35 +51,3 @@ def img_process_fn(image, metadata):
 
 
 
-
-
-
-from pycromanager import Acquisition, multi_d_acquisition_events
-
-# with Acquisition(directory='/Users/henrypinkard/megllandump', name='tcz_acq') as acq:
-#     # Generate the events for a single z-stack
-#     events = multi_d_acquisition_events(
-#         num_time_points=3, time_interval_s=0,
-#         channel_group='channel', channels=['DAPI', 'FITC'],
-#         z_start=0, z_end=6, z_step=0.4,
-#         order='tcz')
-#     acq.acquire(events)
-
-with Acquisition('/Users/henrypinkard/megllandump', 'l_axis') as acq:
-    #create one event for the image at each z-slice
-    events = []
-    for time in range(5):
-        for index, z_um in enumerate(np.arange(start=0, stop=10, step=0.5)):
-            evt = {
-                    #'axes' is required. It is used by the image viewer and data storage to
-                    #identify the acquired image
-                    'axes': {'l': index, 'time': time},
-
-                    #the 'z' field provides the z position in µm
-                    'z': z_um}
-            events.append(evt)
-
-    acq.acquire(events)
-
-
-
