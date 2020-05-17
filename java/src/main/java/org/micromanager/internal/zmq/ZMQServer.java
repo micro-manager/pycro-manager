6a62098f92e13491bb3067ac59e195ca1ce65aab
Henry Pinkard <henry.pinkard@gmail.com>
2020-05-17 06:22:36 -0700

    update ZMQServer internal version

--- java/src/main/java/org/micromanager/internal/zmq/ZMQServer.java
+++ java/src/main/java/org/micromanager/internal/zmq/ZMQServer.java
@@ -29,8 +29,7 @@ public class ZMQServer extends ZMQSocketWrapper {
    private Set<String> packages_;
    private ZMQUtil util_;
 
-   //Copied from magellan version for backwards compatibility, but they are now seperate I guess
-   public static final String VERSION = "2.4.0";
+   public static final String VERSION = "2.5.0";
 
    private final Function<Class, Object> classMapper_;
    private static ZMQServer masterServer_;
