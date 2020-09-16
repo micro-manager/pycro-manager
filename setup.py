bf6e68674dfb828fca77d5dad766bc426f6c9846
Henry Pinkard <henry.pinkard@gmail.com>
2020-09-16 15:40:24 -0700

    bump pip version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pycromanager",
-    version="0.4.10",
+    version="0.4.11",
     author="Henry Pinkard",
     author_email="henry.pinkard@gmail.com",
     description="Open source microscope control using python",
