13d9420b4e2a8975201ef9f847ae017d26404641
Henry Pinkard <henry.pinkard@gmail.com>
2020-09-20 17:08:43 -0500

    trying to fix readthedocs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2020, Henry Pinkard'
 author = 'Henry Pinkard'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+# release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
