b1d0fb7698903841f867d247a05f8a5ee07f0cc3
Henry Pinkard <henry.pinkard@gmail.com>
2021-10-07 13:49:16 -0700

    update function

--- update_libraries.py
+++ update_libraries.py
@@ -1,31 +1,31 @@
+"""
+Script for updating pycormanager and its java libraries
+
+version number in library pom and pycromanager pom must be changed manually
+
+this script updates pycromanager to use latest libraries, maven updates all applicable, 
+then updates version numbers in micromanager
+"""
+
 from pathlib import Path
 import re
 
 # get latest version numbers of NDTiff, AcqEngJ, NDViewer, PycormanagerJava from their repsective files
 versions = {}
 git_repos_dir = Path(__file__).parent.parent 
+poms = {'NDTiffStorage': '/NDTiffStorage/pom.xml',
+		'NDViewer': '/NDViewer/pom.xml'
+		'AcqEngJ': '/AcqEngJ/pom.xml'
+		'PycroManagerJava': '/pycro-manager/java/pom.xml'}
 
-f = str(git_repos_dir) + '/NDTiffStorage/pom.xml'
-with open(f) as pom_file:
-	s = pom_file.read()
-	versions['NDTiffStorage'] = s.split('<version>')[1].split('</version')[0]
-
-f = str(git_repos_dir) + '/NDViewer/pom.xml'
-with open(f) as pom_file:
-	s = pom_file.read()
-	versions['NDViewer'] = s.split('<version>')[1].split('</version')[0]
-
-f = str(git_repos_dir) + '/AcqEngJ/pom.xml'
-with open(f) as pom_file:
-	s = pom_file.read()
-	versions['AcqEngJ'] = s.split('<version>')[1].split('</version')[0]
-
-f = str(git_repos_dir) + '/pycro-manager/java/pom.xml'
-with open(f) as pom_file:
-	s = pom_file.read()
-	versions['PycroManagerJava'] = s.split('<version>')[1].split('</version')[0]
+for lib in poms.keys():
+	f = str(git_repos_dir) + poms[lib]
+	with open(f) as pom_file:
+		s = pom_file.read()
+		versions[lib] = s.split('<version>')[1].split('</version')[0]
 
 
+redeploys = []
 ###### update dependencies in PycroManageJava
 print('Updating PycroManageJava pom')
 f = str(git_repos_dir) + '/pycro-manager/java/pom.xml'
@@ -36,6 +36,8 @@ for lib_name in versions:
 	new_ver = versions[lib_name]
 	allf = re.findall('{}</artifactId>\n.*?<version>(.*?)</version>'.format(lib_name), data, )
 	old_ver = allf[0]
+	if new_ver != old_ver:
+		redeploys.append(lib_name)
 	# for a in allf:
 	# 	print(a)
 	print('{}:\t\tCurrent: {}\tNew: {}'.format(lib_name, old_ver, new_ver))
@@ -46,6 +48,12 @@ for lib_name in versions:
 # Rewrite file
 with open(f, 'w') as outfile:
     outfile.write(data)
+
+
+#maven deploys
+for lib_name in redeploys:
+	folder_name = Path(str(git_repos_dir) + poms[lib]).parent
+	os.system('cd {} && mvn clean && mvn deploy'.format(folder_name))
 
 
 ####### update dependencies in micro-manager
@@ -68,7 +76,6 @@ with open(f, 'w') as outfile:
     outfile.write(data)
 
 
-#clean and build all libraries
 
 
 
