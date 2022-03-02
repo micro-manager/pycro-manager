5e1e13a41930fb83492e3fc2506dfaa50c20ae51
Henry Pinkard <henry.pinkard@gmail.com>
2022-03-02 09:56:05 -0800

    roll back dask version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         "numpy",
-        "dask>=2022.2.1",
+        "dask>=2022.2.0",
         "zmq",
         "docstring-inheritance",
     ],
