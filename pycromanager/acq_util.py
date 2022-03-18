691eff757c8171d838b6c4a33b078f2d036fde92
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2022-03-17 20:46:30 -0400

    pass kwargs to core and return a core in headless

--- pycromanager/acq_util.py
+++ pycromanager/acq_util.py
@@ -20,8 +20,8 @@ atexit.register(cleanup)
 
 def start_headless(
     mm_app_path: str, config_file: str, java_loc: str=None, core_log_path: str=None, buffer_size_mb: int=1024,
-        port: int=Bridge.DEFAULT_PORT, timeout: int=5000
-):
+        port: int=Bridge.DEFAULT_PORT, timeout: int=5000, **core_kwargs
+)->Core:
     """
     Start a Java process that contains the neccessary libraries for pycro-manager to run,
     so that it can be run independently of the Micro-Manager GUI/application. This call
@@ -34,21 +34,28 @@ def start_headless(
     Installing Java 11 is the most likely version to work without issue
 
     Parameters
-        ----------
-        mm_app_path : str
-            Path to top level folder of Micro-Manager installation (made with graphical installer)
-        config_file : str
-            Path to micro-manager config file, with which core will be initialized
-        java_loc: str
-            Path to the java version that it should be run with
-        core_log_path : str
-            Path to where core log files should be created
-        buffer_size_mb : int
-            Size of circular buffer in MB in MMCore
-        port : int
-            Default port to use for ZMQServer
-        timeout : int, default 500
-            Timeout for connection to server in milliseconds
+    ----------
+    mm_app_path : str
+        Path to top level folder of Micro-Manager installation (made with graphical installer)
+    config_file : str
+        Path to micro-manager config file, with which core will be initialized
+    java_loc: str
+        Path to the java version that it should be run with
+    core_log_path : str
+        Path to where core log files should be created
+    buffer_size_mb : int
+        Size of circular buffer in MB in MMCore
+    port : int
+        Default port to use for ZMQServer
+    timeout : int, default 5000
+        Timeout for connection to server in milliseconds
+    **core_kwargs
+        Passed on to the Core constructor
+
+    Returns
+    -------
+    core :
+        A Core object
     """
 
     classpath = mm_app_path + '/plugins/Micro-Manager/*'
@@ -79,7 +86,7 @@ def start_headless(
     )
 
     # Initialize core
-    core = Core(timeout=timeout)
+    core = Core(timeout=timeout, **core_kwargs)
 
     core.wait_for_system()
     core.load_system_configuration(config_file)
@@ -90,6 +97,7 @@ def start_headless(
         core.enable_stderr_log(True)
         core.enable_debug_log(True)
         core.set_primary_log_file(core_log_path)
+    return core
 
 
 
