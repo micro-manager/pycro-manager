2d01827abf9613b6d5f752c28c68a3f74b4e9d75
ieivanov <ivan.ivanov@czbiohub.org>
2022-12-01 11:52:25 -0800

    add tests

--- pycromanager/test/test_acquisition.py
+++ pycromanager/test/test_acquisition.py
@@ -54,3 +54,27 @@ def test_zstack_seq_acq(launch_mm_headless, setup_data_folder):
                      pre_hardware_hook_fn=hook_fn) as acq:
         acq.acquire(events)
 
+
+def test_multi_d_acq(launch_mm_headless, setup_data_folder):
+    events = multi_d_acquisition_events(
+        num_time_points=10,
+        time_interval_s=0,
+        channel_group="Channel",
+        channels=["DAPI", "FITC"],
+        z_start=0,
+        z_end=5,
+        z_step=1,
+        order="tcz",
+    )
+
+    with Acquisition(setup_data_folder, 'acq', show_display=False) as acq:
+        acq.acquire(events)
+        dataset = acq.get_dataset()
+
+    for t in range(10):
+        for z in range(6):
+            for ch in ["DAPI", "FITC"]:
+                assert dataset.has_image(time=t, channel_name=ch, z=z)
+
+    data = dataset.as_array(axes=['time', 'channel', 'z'])
+    assert data.shape[:-2] == (10, 2, 6)
