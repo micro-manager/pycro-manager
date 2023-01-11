07336a1e01fd8c89089f21151e8595b144ac256f
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2023-01-10 19:08:06 -0800

    fix bug

--- pycromanager/test/test_viewer.py
+++ pycromanager/test/test_viewer.py
@@ -5,7 +5,7 @@ from pycromanager import Acquisition, Core, multi_d_acquisition_events
 # Skip tests in this module if it is running in GitHub Actions, which does not support NDViewer
 # https://docs.pytest.org/en/7.1.x/how-to/skipping.html
 # https://docs.github.com/ko/actions/learn-github-actions/variables
-pytestmark = pytest.mark.skipif(os.getenv('GITHUB_ACTIONS') is True)
+pytestmark = pytest.mark.skipif(os.getenv('GITHUB_ACTIONS') == 'true')
 
 def test_multi_d_acq_viewer(launch_mm_headless, setup_data_folder):
     events = multi_d_acquisition_events(
