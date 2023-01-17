446d0328770e3e7d0dc21ebf29f7ff128b6ed796
Henry Pinkard <7969470+henrypinkard@users.noreply.github.com>
2023-01-16 16:11:54 -0800

    Update update_mm_ivy.py

--- build_automation/update_mm_ivy.py
+++ build_automation/update_mm_ivy.py
@@ -5,6 +5,7 @@ It runs upon changes to the java/pom.xml file of the main branch
 """
 
 import xml.etree.ElementTree as ET
+from lxml import etree
 from semantic_version import Version
 from pathlib import Path
 import requests
@@ -33,33 +34,36 @@ root = tree.getroot()
 updated_versions = read_versions(root)
 
 
-tree = ET.parse(ivy_path)
-root = tree.getroot()
+def modify_rev(xml_str, updated_versions):
+    root = etree.fromstring(xml_str)
+    dependencies = {
+        "org.micro-manager.ndviewer": {"name":"NDViewer"},
+        "org.micro-manager.acqengj": {"name":"AcqEngJ"},
+        "org.micro-manager.ndtiffstorage": {"name":"NDTiffStorage"},
+        "org.micro-manager.pycro-manager": {"name":"PycroManagerJava"},
+    }
 
-dependencies = {
-    "org.micro-manager.ndviewer": {"name":"NDViewer"},
-    "org.micro-manager.acqengj": {"name":"AcqEngJ"},
-    "org.micro-manager.ndtiffstorage": {"name":"NDTiffStorage"},
-    "org.micro-manager.pycro-manager": {"name":"PycroManagerJava"},
-}
-
-
-
-for dependency in root.iter("dependency"):
-    if "org" not in dependency.attrib:
-        continue
-    org = dependency.attrib["org"]
-    name = dependency.attrib["name"]
-    if org in dependencies and name == dependencies[org]["name"]:
-        new_version = str(updated_versions[dependencies[org]["name"]])
-        print(dependencies[org]["name"], '\t', dependency.attrib["rev"],  'to\t', new_version)
-        dependency.attrib["rev"] = new_version
+    for dependency in root.iter("dependency"):
+        if "org" not in dependency.attrib:
+            continue
+        org = dependency.attrib["org"]
+        name = dependency.attrib["name"]
+        if org in dependencies and name == dependencies[org]["name"]:
+            new_version = str(updated_versions[dependencies[org]["name"]])
+            print(dependencies[org]["name"], '\t', dependency.attrib["rev"],  'to\t', new_version)
+            dependency.attrib["rev"] = new_version
+    return etree.tostring(root, pretty_print=True).decode()
 
 
-tree.write(ivy_path)
+with open(ivy_path, 'r') as f:
+    xml_str = f.read()
 
+modified_xml_str = modify_rev(xml_str, updated_versions)
 
-
+with open(ivy_path, 'w') as f:
+    f.write(modified_xml_str)
+    
+    
 # Wait for PycroManagerJava to become available, because there is a delay after it is deployed
 dep_name = 'PycroManagerJava'
 latest_version_number = str(updated_versions[dep_name])
