427c59ab895c857490a4fc23470c0ee4d24bd1a1
ieivanov <ivan.ivanov@czbiohub.org>
2023-01-31 17:05:59 -0800

    try running test_multi_d_acq_NDViewer in GitHub Actions

--- pycromanager/test/test_viewer.py
+++ pycromanager/test/test_viewer.py
@@ -6,7 +6,7 @@ import napari
 # Skip tests in this module if it is running in GitHub Actions, which does not support NDViewer
 # https://docs.pytest.org/en/7.1.x/how-to/skipping.html
 # https://docs.github.com/ko/actions/learn-github-actions/variables
-pytestmark = pytest.mark.skipif(os.getenv('GITHUB_ACTIONS') == 'true', reason="NDViewer does not work in GitHub Actions")
+# pytestmark = pytest.mark.skipif(os.getenv('GITHUB_ACTIONS') == 'true', reason="NDViewer does not work in GitHub Actions")
 
 def test_multi_d_acq_NDViewer(launch_mm_headless, setup_data_folder):
     events = multi_d_acquisition_events(
@@ -27,6 +27,7 @@ def test_multi_d_acq_NDViewer(launch_mm_headless, setup_data_folder):
     acq.get_viewer().close()
 
 
+@pytest.mark.skipif(os.getenv('GITHUB_ACTIONS') == 'true', reason="NDViewer does not work in GitHub Actions")
 def test_multi_d_acq_napari_viewer(launch_mm_headless, setup_data_folder):
     events = multi_d_acquisition_events(
         num_time_points=10,
