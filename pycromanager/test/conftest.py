1b89e0bcbda62e3d2405df3f7d15fc20daefc323
Henry Pinkard <7969470+henrypinkard@users.noreply.github.com>
2024-04-08 19:00:51 -0700

    make sure to not get wrong java location

--- pycromanager/test/conftest.py
+++ pycromanager/test/conftest.py
@@ -183,9 +183,9 @@ def launch_mm_headless(install_mm):
         config_file = os.path.join(mm_install_dir, 'MMConfig_demo.cfg')
         print('Launching Micro-manager in headless mode.')
 
-        # MM doesn't ship with Java on Mac so allow it to be defined here
+        # MM doesn't ship with Java on Mac so allow it to be defined here if using mac os
         java_loc = None
-        if "JAVA" in os.environ:
+        if "JAVA" in os.environ and sys.platform == "darwin":
             java_loc = os.environ["JAVA"]
 
         start_headless(mm_install_dir, config_file, java_loc=java_loc, debug=True)
