bce90fb53818e38123da08756bd41baebe75074c
Henry Pinkard <7969470+henrypinkard@users.noreply.github.com>
2024-06-25 09:12:14 +0200

    progress

---
+++ pycromanager/acquisition/new/test/t3st_event_execution.py
@@ -0,0 +1,64 @@
+import time
+
+from pycromanager import start_headless
+from pycromanager.acquisition.new.image_coords import ImageCoordinates
+from pycromanager.acquisition.new.mm_device_implementations import MicroManagerCamera
+import os
+from pycromanager.acquisition.new.executor import AcquisitionEventExecutor
+from pycromanager.acquisition.new.acq_events import StartCapture, ReadoutImages, DataOutputQueue
+
+
+mm_install_dir = '/Users/henrypinkard/Micro-Manager'
+config_file = os.path.join(mm_install_dir, 'MMConfig_demo.cfg')
+start_headless(mm_install_dir, config_file,
+               buffer_size_mb=1024, max_memory_mb=1024,  # set these low for github actions
+               python_backend=True,
+               debug=False)
+
+
+
+executor = AcquisitionEventExecutor()
+
+
+
+camera = MicroManagerCamera()
+
+
+num_images = 100
+data_output_queue = DataOutputQueue()
+
+start_capture_event = StartCapture(num_images=num_images, camera=camera)
+readout_images_event = ReadoutImages(num_images=num_images, camera=camera,
+                                     image_coordinate_iterator=[ImageCoordinates(time=t) for t in range(num_images)],
+                                     output_queue=data_output_queue)
+
+executor.submit_event(start_capture_event)
+executor.submit_event(readout_images_event)
+
+image_count = 0
+while True:
+    coordinates, image, metadata = data_output_queue.get()
+    image_count += 1
+    print(f"Got image {image_count}  ", f'pixel mean {image.mean()}' )
+    if image_count == num_images:
+        break
+
+executor.shutdown()
+
+
+#
+# events = []
+# coord_list = [ImageCoordinates(time=t) for t in range(10)]
+# for coord in coord_list:
+#     events.append(ReadoutImages(num_images=1, camera=camera, image_coordinate_iterator=coord_list))
+#
+# with Acquisition(show_display=False, debug=True) as acq:
+#     acq.acquire(events)
+
+
+
+#
+# with Acquisition(show_display=False, debug=True) as acq:
+#     # copy list of events to avoid popping from original
+#     acq.acquire(multi_d_acquisition_events(num_time_points=10))
+
